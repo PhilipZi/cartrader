7606f6d4b02afb112a3190ccec72fc286d341887
Philip Zimmermann <pzi@ito-consult.de>
2023-06-15 19:06:30 +0200

    add Cars page

--- app.vue
+++ app.vue
@@ -1,23 +1,49 @@
 <template>
-  <div class="">
-    <!-- NAVBAR -->
+  <div>
     <header
       class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md"
     >
       <a class="text-3xl font-mono" href="">Cartrader</a>
     </header>
-    <!-- NAVBAR -->
 
-    <!-- HOME HERO -->
+    <!-- CARS PAGE -->
     <div
-      class="relative h-screen w-100 bg-no-repeat bg-cover bg-bottom bg-[url('https://imgr1.auto-motor-und-sport.de/Ferrari-F8-Tributo-article169Gallery-e8a33bc7-1432047.jpg')]"
+      class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg: px-16 pb-16 w-3/5"
     >
-      <div class="z-10 absolute w-full h-full flex justify-center items-center">
-        <div class="text-center w-1/2">
-          <h1 class="text-8xl font-serif mb-10 text-white">Find your drive</h1>
+      <div class="mt-32 flex">
+        <!-- CARS SIDE BAR -->
+        <div class="shadow border w-64 mr-10 z-30 h-[190px]">
+          <div
+            class="p-5 flex justify-between relative cursor-pointer border-b"
+          >
+            <h3>Location</h3>
+            <h3 class="text-blue-400 capitalize">Toronto</h3>
+            <!-- <div class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white">
+              <input type="text" class="border p1 rounded" />
+              <button class="bg-blue-400 w-full mt-2 rounded text-white p-1">
+                Apply
+              </button>
+            </div> -->
+          </div>
+          <div
+            class="p-5 flex justify-between relative cursor-pointer border-b"
+          >
+            <h3>Make</h3>
+            <h3 class="text-blue-400 capitalize">Any</h3>
+          </div>
+          <div
+            class="p-5 flex justify-between relative cursor-pointer border-b"
+          >
+            <h3>Price</h3>
+            <h3 class="text-blue-400 capitalize">Any</h3>
+          </div>
         </div>
+        <!-- CARS SIDE BAR -->
+
+        <!-- CARS CARDS -->
+        <!-- CARS CARDS -->
       </div>
     </div>
-    <!-- HOME HERO -->
+    <!-- CARS PAGE -->
   </div>
 </template>
