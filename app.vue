f6dd8b8a16d2db4de3d13acc3cef9e3325305603
Philip Zimmermann <pzi@ito-consult.de>
2023-06-15 18:30:33 +0200

    add header to App component

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- NAVBAR -->
+    <header
+      class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md"
+    >
+      <a class="text-3xl font-mono" href="">Cartrader</a>
+    </header>
+    <!-- NAVBAR -->
   </div>
 </template>
