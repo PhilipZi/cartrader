d92b1560d3648f45a6690a6459e179094bad31a7
Philip Zimmermann <pzi@ito-consult.de>
2023-08-15 16:39:07 +0200

    add nested routes

--- pages/city/[city]/car/[[make]].vue
+++ pages/city/[city]/car/[[make]].vue
@@ -1,18 +1,5 @@
 <template>
   <div>
-    <NavBar />
-    <div
-      class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg: px-16 pb-16 w-3/5"
-    >
-      <div class="mt-32 flex">
-        <CarSideBar />
-        <CarCards />
-      </div>
-    </div>
-    <!-- CARS PAGE -->
+    <CarCard />
   </div>
 </template>
-
-<script setup></script>
-
-<style lang="scss" scoped></style>
