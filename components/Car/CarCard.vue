011691dd588c626b6c0ee363137d15c953451b1f
Philip Zimmermann <pzi@ito-consult.de>
2023-08-16 13:56:03 +0200

    add NuxtImg lib and use it on all img elements

--- components/Car/CarCard.vue
+++ components/Car/CarCard.vue
@@ -4,7 +4,7 @@
     @click.prevent="navigateTo(`/car/${car.name}-${car.id}`)"
   >
     <div class="flex h-full">
-      <img :src="car.url" alt="" class="w-[350px] h-full" />
+      <NuxtImg :src="car.url" alt="" class="w-[350px] h-full" />
       <div class="p-4 flex flex-col">
         <div>
           <h1 class="text-2xl text-blue-700">{{ car.name }}</h1>
