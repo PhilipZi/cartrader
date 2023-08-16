a01eae9018f62472198c14a57c22e85b807f568d
Philip Zimmermann <pzi@ito-consult.de>
2023-08-16 10:33:00 +0200

    add dynamic error message and statuses via useError

--- error.vue
+++ error.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex h-screen justify-center items-center flex-col">
-    <h1 class="text-9xl">404</h1>
-    <p class="mt-7 text-4xl">Page not found!</p>
+    <h1 class="text-9xl">{{ error.statusCode }}</h1>
+    <p class="mt-7 text-4xl">{{ error.message }}</p>
     <button
       class="rounded mt-7 text-2xl bg-blue-400 px-7 py-4 text-white"
       @click.prevent="handleGoBack"
@@ -12,7 +12,7 @@
 </template>
 
 <script setup>
-const router = useRouter();
+const error = useError();
 
 const handleGoBack = () => {
   navigateTo("/");
